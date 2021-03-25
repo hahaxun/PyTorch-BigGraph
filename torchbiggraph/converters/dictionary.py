206c4920c890fb012b3c17c0b4693ec03ebde4e1
root <root@bjlt-hg34.sy>
2021-03-26 00:11:01 +0800

    [TAG]add nlp support

--- torchbiggraph/converters/dictionary.py
+++ torchbiggraph/converters/dictionary.py
@@ -51,7 +51,7 @@ class Dictionary:
 
 class PartDictionary(Dictionary):
     def __init__(self, ix_to_word: List[str], num_parts: int = 1, part_:int = 0) -> None:
-        super.__init__(ix_to_word, num_parts)
+        super().__init__(ix_to_word, num_parts)
         self.part_ = part_
     
     def size(self) -> int:
